d71093f1e7bf784648d832cd597cd39454012ca8
Marcos Morais <marcosmorais.v10@gmail.com>
2019-02-13 19:54:00 -0200

    alteracao e criacao de componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+import { Student } from './student/student.model'
+
 @Component({
   selector: 'jad-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'academy-jedi';
+
+  students: Student[] = [
+    {name: 'Luke', isJedi: true, temple: 'Coruscant'},
+    {name: 'Han Solo', isJedi: false},
+    {name: 'Obi-Wan-Kenobi', isJedi: true, temple: 'Coruscant'},
+    {name: 'Anakin Skywalker', isJedi: true, temple: 'Coruscant'},
+    {name: 'Leia', isJedi: false}
+
+  ]
+  /*luke = {name: 'Luke', isJedi: true, temple: 'Coruscant'}
+  han = {name: 'Han Solo', isJedi: false}
+  obi = {name: 'Obi-Wan-Kenobi', isJedi: true, temple: 'Coruscant'}
+  anakin = {name: 'Anakin Skywalker', isJedi: true, temple: 'Coruscant'}
+  leia = {name: 'Leia', isJedi: false}*/
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { StudentComponent } from './student/student.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule,
